ca666109473a792078b7e110e661ee3925be7c88
Ali008009 <152096514+Ali008009@users.noreply.github.com>
2025-01-21 17:30:28 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,4 @@
+// JavaScript to handle button click for "Learn More"
+document.getElementById('moreInfoBtn').addEventListener('click', function() {
+    alert('This will take you to the About section.');
+});
